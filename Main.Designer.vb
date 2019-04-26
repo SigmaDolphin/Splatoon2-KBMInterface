017b667c4486d6795d852cbd06006119e35b9796
SigmaDolphin <49135857+SigmaDolphin@users.noreply.github.com>
2019-04-25 19:04:02 -0500

    added main files

---
+++ Main.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Main
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.BaudRate = 115200
+        Me.SerialPort1.PortName = "COM6"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 30)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Label1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 119)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(39, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Label3"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 147)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Label4"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(435, 119)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Label5"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(435, 147)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(39, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Label6"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(364, 30)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "Label7"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(12, 207)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(126, 104)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "Button A - E" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button B - Spacebar" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button Y - Enter" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button X - Tab" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button L - 3" & _
+            "" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button R - RMouse" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button ZL - Shift (Toggle)" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button ZR - LMouse" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(144, 207)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(93, 78)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "Button - - H" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button + - L" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "LClick - J" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "RClick - R" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button Home - B" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "Button Captu" & _
+            "re - 1"
+        '
+        'Main
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(486, 342)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Main"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+
+End Class
---
+++ Main.vb
@@ -0,0 +1,277 @@
+﻿Option Explicit On
+Public Class Main
+    Dim mouseX As Integer
+    Dim mouseY As Integer
+    Dim xAdv As Integer
+    Dim yAdv As Integer
+    Dim comResponse As String
+    Dim byteOut(8) As Byte
+
+    Dim buttonA As Byte
+    Dim buttonB As Byte
+    Dim LX As Byte
+    Dim LY As Byte
+    Dim RX As Byte
+    Dim RY As Byte
+    Dim padD As Byte
+
+    'Dim a As String
+    'Dim file As System.IO.StreamWriter
+
+    Dim lastKeyDown As Byte
+
+    Dim comThread As New System.Threading.Thread(AddressOf byteSender)
+
+    Private Sub Main_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseMove
+        mouseX = Cursor.Position.X
+        mouseY = Cursor.Position.Y
+    End Sub
+
+    Private Sub Main_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = False
+        Timer1.Enabled = False
+        'file = My.Computer.FileSystem.OpenTextFileWriter("c:\WebCam\test.txt", False)
+        LX = 128
+        LY = 128
+        padD = 8
+    End Sub
+
+    Sub Main_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseDown
+        Select Case e.Button
+            'button ZR
+            Case MouseButtons.Left
+                buttonA = buttonA + 128
+                'button R
+            Case MouseButtons.Right
+                buttonA = buttonA + 32
+        End Select
+    End Sub
+
+    Sub Main_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseUp
+        Select Case e.Button
+            Case MouseButtons.Left
+                buttonA = buttonA - 128
+            Case MouseButtons.Right
+                buttonA = buttonA - 32
+        End Select
+    End Sub
+
+    Private Sub Main_KeyDown(ByVal sender As Object, ByVal e As KeyEventArgs) Handles MyBase.KeyDown
+        If lastKeyDown <> e.KeyCode Then
+            lastKeyDown = e.KeyCode
+            Select Case e.KeyCode
+                'locks/unlocks mouse
+                Case Keys.D0
+                    If Timer1.Enabled = False Then
+                        Timer1.Enabled = True
+                        SerialPort1.Open()
+                        comThread.Start()
+
+                    Else
+                        Timer1.Enabled = False
+                        comThread.Abort()
+                        comThread = New System.Threading.Thread(AddressOf byteSender)
+                        SerialPort1.Close()
+                        'file.WriteLine(a)
+                    End If
+                    'direction movement, left stick
+                Case Keys.W
+                    LY = 0
+                Case Keys.A
+                    LX = 0
+                Case Keys.S
+                    LY = 255
+                Case Keys.D
+                    LX = 255
+
+                    'button B
+                Case Keys.Space
+                    buttonA = buttonA + 2
+                    'button Y
+                Case Keys.Enter
+                    buttonA = buttonA + 1
+                    'button X
+                Case Keys.Tab
+                    e.SuppressKeyPress = True
+                    buttonA = buttonA + 8
+                    'button L
+                Case Keys.D3
+                    buttonA = buttonA + 16
+                    'button A
+                Case Keys.E
+                    buttonA = buttonA + 4
+                    'button ZL
+                Case Keys.ShiftKey
+                    If buttonA And 64 Then
+                        buttonA = buttonA - 64
+                    Else
+                        buttonA = buttonA + 64
+                    End If
+
+
+
+
+
+                    'button - 
+                Case Keys.H
+                    buttonB = buttonB + 1
+                    'button +
+                Case Keys.L
+                    buttonB = buttonB + 2
+                    'L click
+                Case Keys.J
+                    buttonB = buttonB + 4
+                    'button Rclick
+                Case Keys.R
+                    buttonB = buttonB + 8
+                    'home
+                Case Keys.B
+                    buttonB = buttonB + 16
+                    'capture
+                Case Keys.D1
+                    buttonB = buttonB + 32
+
+                Case Keys.Up
+                    padD = 0
+                Case Keys.Down
+                    padD = 4
+                Case Keys.Left
+                    padD = 6
+                Case Keys.Right
+                    padD = 2
+            End Select
+        End If
+    End Sub
+
+    Private Sub Main_KeyUp(ByVal sender As Object, ByVal e As KeyEventArgs) Handles MyBase.KeyUp
+        Select Case e.KeyCode
+            'direction movement, left stick
+            Case Keys.W
+                LY = 128
+            Case Keys.A
+                If LX = 0 Then
+                    LX = 128
+                End If
+            Case Keys.S
+                LY = 128
+            Case Keys.D
+                If LX = 255 Then
+                    LX = 128
+                End If
+
+                'button B
+            Case Keys.Space
+                buttonA = buttonA - 2
+                'button Y
+            Case Keys.Enter
+                buttonA = buttonA - 1
+                'button X
+            Case Keys.Tab
+                e.SuppressKeyPress = True
+                buttonA = buttonA - 8
+                'button L
+            Case Keys.D3
+                buttonA = buttonA - 16
+                'button R
+            Case Keys.E
+                buttonA = buttonA - 4
+                'button ZL
+                'Case Keys.ShiftKey
+                'buttonA = buttonA - 64
+
+
+
+
+
+                'button - 
+            Case Keys.H
+                buttonB = buttonB - 1
+                'button +
+            Case Keys.L
+                buttonB = buttonB - 2
+                'L click
+            Case Keys.J
+                buttonB = buttonB - 4
+                'button Rclick
+            Case Keys.R
+                buttonB = buttonB - 8
+                'home
+            Case Keys.B
+                buttonB = buttonB - 16
+                'capture
+            Case Keys.D1
+                buttonB = buttonB - 32
+
+            Case Keys.Up
+                padD = 8
+            Case Keys.Down
+                padD = 8
+            Case Keys.Left
+                padD = 8
+            Case Keys.Right
+                padD = 8
+
+        End Select
+        If lastKeyDown = e.KeyCode Then
+            'The last key has been released so forget it.
+            lastKeyDown = 0
+        End If
+    End Sub
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+
+        xAdv = 128 - (Screen.PrimaryScreen.Bounds.Width / 2 - mouseX) * 3
+        yAdv = 128 - (Screen.PrimaryScreen.Bounds.Height / 2 - mouseY) * 3.5
+        RX = setLimit(xAdv)
+        RY = setLimit(yAdv)
+        If RY < 128 And RY > 108 Then
+            RY = 108
+        End If
+        If RY > 128 And RY < 148 Then
+            RY = 148
+        End If
+        Cursor.Position = New Point(Screen.PrimaryScreen.Bounds.Width / 2, Screen.PrimaryScreen.Bounds.Height / 2)
+        Label1.Text = LX
+        Label2.Text = LY
+        Label3.Text = RX
+        Label4.Text = RY
+        Label5.Text = buttonA
+        Label6.Text = buttonB
+
+        byteOut(0) = &H53
+        byteOut(1) = buttonA
+        byteOut(2) = buttonB
+        byteOut(3) = RX
+        byteOut(4) = RY
+        byteOut(5) = LX
+        byteOut(6) = LY
+        byteOut(7) = padD
+        byteOut(8) = 0
+
+    End Sub
+
+
+    Sub byteSender()
+        Do
+            'Label7.Text = ""
+            'comResponse = SerialPort1.ReadExisting
+            'Label7.Text = comResponse
+            'a = a & comResponse
+            SerialPort1.Write(byteOut, 0, 9)
+        Loop
+    End Sub
+
+    Function setLimit(ByVal valu As Integer)
+        If valu > 255 Then
+            setLimit = 255
+        ElseIf valu < 0 Then
+            setLimit = 0
+        ElseIf valu = 83 Then
+            setLimit = 84
+        Else
+            setLimit = valu
+        End If
+    End Function
+
+End Class
